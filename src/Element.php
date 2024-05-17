0c35a05a65963e92861654cdc5f4045a8734aee6
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2024-05-17 18:05:13 +0100

    docs

--- src/Element.php
+++ src/Element.php
@@ -5,10 +5,23 @@ namespace Drupal\declarative_form_ajax;
 use Drupal\Core\Render\Element as RenderElement;
 
 /**
- * Provides helpers TODO.
+ * Provides helpers for working with form elements.
+ *
+ * @todo This should be merged with core's Element class.
  */
 class Element {
 
+  /**
+   * Applies a callback to an element and all of its children at all depths.
+   *
+   * @todo: Consider allowing additional callback parameters to be passed in to
+   * this method when adding it to core.
+   *
+   * @param array $element
+   *   The form element.
+   * @param callable $callback
+   *   The callback to apply.
+   */
   public static function walkChildrenRecursive(array $element, callable $callback) {
     // Call the callback on the element.
     $callback($element);
